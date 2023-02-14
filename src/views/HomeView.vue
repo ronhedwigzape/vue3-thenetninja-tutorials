713a28e173d08c0dd856fe6dbd8a138e34d40d61
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-14 16:42:01 +0800

    Learn Fetching Data in setup (asynchronous code)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,26 +16,50 @@
 <!--  <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>-->
 <!--  <button @click="updateNinjaTwo">Update Ninja Two</button>-->
 
+<!--  <h1>Home</h1>-->
+<!--  <input type="text" v-model="search">-->
+<!--  <p>search term - {{ search }}</p>-->
+<!--  <div v-for="name in matchingNames" :key="name">-->
+<!--    {{ name }}-->
+<!--  </div>-->
+<!--  <button @click="handleClick">Stop Watching</button>-->
+
   <h1>Home</h1>
-  <input type="text" v-model="search">
-  <p>search term - {{ search }}</p>
-  <div v-for="name in matchingNames" :key="name">
-    {{ name }}
+  <div v-if="error">{{ error }}</div>
+  <div v-if="posts.length">
+    <PostList v-if="showPosts" :posts="posts" />
   </div>
-  <button @click="handleClick">Stop Watching</button>
-
-
-
+  <div v-else>Loading...</div>
+  <button @click="showPosts = !showPosts">toggle posts</button>
+  <button @click="posts.pop()">delete a post</button>
 </div>
 </template>
 <script>
-
+import PostList from "../components/PostList.vue";
 import {ref, reactive, computed, watch, watchEffect} from "vue";
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
+    const posts = ref([])
+    const error = ref(null)
 
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(err.value)
+      }
+    }
+  load()
+    const showPosts = ref(true)
 
     // // Watch and Watch Effect
     // const search = ref('')
@@ -90,6 +114,9 @@ export default {
     //   age.value = 35
     // }
     return {
+      posts,
+      showPosts,
+      error
       // names,
       // search,
       // matchingNames,
